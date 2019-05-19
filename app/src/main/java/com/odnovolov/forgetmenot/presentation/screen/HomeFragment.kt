89a49cf8d8032d4766646407e8da0656ff61720b
Odnovolov Artem <odnovolov.artem@gmail.com>
2019-05-19 18:52:13 +0300

    add 'Add new deck' toolbar button

--- app/src/main/java/com/odnovolov/forgetmenot/presentation/screen/HomeFragment.kt
+++ app/src/main/java/com/odnovolov/forgetmenot/presentation/screen/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.odnovolov.forgetmenot.presentation.screen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 
 import com.odnovolov.forgetmenot.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -15,6 +18,35 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        toolbar.inflateMenu(R.menu.home_actions)
+        toolbar.setOnMenuItemClickListener { item: MenuItem? ->
+            when (item?.itemId) {
+                R.id.action_add -> {
+                    showFileChooser()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+            .addCategory(Intent.CATEGORY_OPENABLE)
+            .setType("text/plain")
+        startActivityForResult(intent, GET_CONTENT_REQUEST_CODE)
+    }
+
+    companion object {
+        const val GET_CONTENT_REQUEST_CODE = 39
     }
 
 }
