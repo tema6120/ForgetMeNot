78392ceaabd716803ccf8b5e7400d9f1585fc340
Odnovolov Artem <odnovolov.artem@gmail.com>
2020-01-11 21:32:34 +0200

    realize 'hint as mask letters' in AnswerOffTestFragment

--- app/src/main/kotlin/com/odnovolov/forgetmenot/screen/exercise/exercisecard/answer/off/View.kt
+++ app/src/main/kotlin/com/odnovolov/forgetmenot/screen/exercise/exercisecard/answer/off/View.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.odnovolov.forgetmenot.R
 import com.odnovolov.forgetmenot.common.base.BaseFragment
+import com.odnovolov.forgetmenot.common.dp
 import com.odnovolov.forgetmenot.screen.exercise.exercisecard.answer.off.AnswerOffTestEvent.*
 import kotlinx.android.synthetic.main.fragment_answer_off_test.*
+import kotlinx.coroutines.flow.combine
 
 class AnswerOffTestFragment : BaseFragment() {
     companion object {
@@ -54,12 +57,45 @@ class AnswerOffTestFragment : BaseFragment() {
     private fun observeViewModel() {
         with(viewModel) {
             answer.observe(onChange = answerTextView::setText)
-            isAnswered.observe { isAnswered: Boolean ->
-                showAnswerButton.visibility = if (isAnswered) GONE else VISIBLE
-            }
+            hint.observe(onChange = hintTextView::setText)
+            isAnswered.combine(hint) { isAnswered, hint -> isAnswered to hint }
+                .observe {
+                    val (isAnswered, hint) = it
+                    val hasHint = hint != null
+                    when {
+                        isAnswered -> {
+                            answerScrollView.visibility = VISIBLE
+                            hintScrollView.visibility = GONE
+                            divider.visibility = GONE
+                            showAnswerButton.visibility = GONE
+                        }
+                        hasHint -> {
+                            answerScrollView.visibility = GONE
+                            hintScrollView.visibility = VISIBLE
+                            divider.visibility = VISIBLE
+                            with(showAnswerButton) {
+                                visibility = VISIBLE
+                                layoutParams.height = 50.dp
+                                requestLayout()
+                            }
+                        }
+                        else -> {
+                            answerScrollView.visibility = GONE
+                            hintScrollView.visibility = GONE
+                            divider.visibility = GONE
+                            with(showAnswerButton) {
+                                visibility = VISIBLE
+                                layoutParams.height = LinearLayout.LayoutParams.MATCH_PARENT
+                                requestLayout()
+                            }
+                        }
+                    }
+                }
             isLearned.observe { isLearned: Boolean ->
                 answerScrollView.isEnabled = !isLearned
                 answerTextView.isEnabled = !isLearned
+                hintScrollView.isEnabled = !isLearned
+                hintTextView.isEnabled = !isLearned
                 showAnswerButton.isEnabled = !isLearned
                 showAnswerTextView.isEnabled = !isLearned
             }
--- app/src/main/kotlin/com/odnovolov/forgetmenot/screen/exercise/exercisecard/answer/off/ViewModel.kt
+++ app/src/main/kotlin/com/odnovolov/forgetmenot/screen/exercise/exercisecard/answer/off/ViewModel.kt
@@ -2,9 +2,11 @@ package com.odnovolov.forgetmenot.screen.exercise.exercisecard.answer.off
 
 import com.odnovolov.forgetmenot.common.database.asFlow
 import com.odnovolov.forgetmenot.common.database.database
+import com.odnovolov.forgetmenot.common.database.mapToOne
 import com.odnovolov.forgetmenot.common.database.mapToOneNotNull
 import com.odnovolov.forgetmenot.screen.exercise.exercisecards.ExerciseCardViewModelQueries
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class AnswerOffTestViewModel(id: Long) {
     private val queries: ExerciseCardViewModelQueries = database.exerciseCardViewModelQueries
@@ -18,6 +20,12 @@ class AnswerOffTestViewModel(id: Long) {
         .isAnswered(id)
         .asFlow()
         .mapToOneNotNull()
+
+    val hint: Flow<String?> = queries
+        .getHint(id)
+        .asFlow()
+        .mapToOne()
+        .map { it.hint }
 
     val isLearned: Flow<Boolean> = queries
         .isLearned(id)
