88ca58667a02f027b6623c0961b1fc8d848fdae1
Odnovolov Artem <odnovolov.artem@gmail.com>
2021-01-04 15:26:17 +0200

    improved interaction with timer value input

--- app/src/main/kotlin/com/odnovolov/forgetmenot/presentation/screen/motivationaltimer/MotivationalTimerController.kt
+++ app/src/main/kotlin/com/odnovolov/forgetmenot/presentation/screen/motivationaltimer/MotivationalTimerController.kt
@@ -9,6 +9,9 @@ import com.odnovolov.forgetmenot.presentation.common.ShortTermStateProvider
 import com.odnovolov.forgetmenot.presentation.common.base.BaseController
 import com.odnovolov.forgetmenot.presentation.screen.help.HelpArticle
 import com.odnovolov.forgetmenot.presentation.screen.help.HelpDiScope
+import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerController.Command
+import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerController.Command.ShowInvalidEntryError
+import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerController.Command.ShowSavedMessage
 import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerEvent.*
 
 class MotivationalTimerController(
@@ -18,7 +21,12 @@ class MotivationalTimerController(
     private val navigator: Navigator,
     private val longTermStateSaver: LongTermStateSaver,
     private val screenStateProvider: ShortTermStateProvider<MotivationalTimerScreenState>
-) : BaseController<MotivationalTimerEvent, Nothing>() {
+) : BaseController<MotivationalTimerEvent, Command>() {
+    sealed class Command {
+        object ShowInvalidEntryError : Command()
+        object ShowSavedMessage : Command()
+    }
+
     override fun handle(event: MotivationalTimerEvent) {
         when (event) {
             HelpButtonClicked -> {
@@ -40,9 +48,13 @@ class MotivationalTimerController(
                 val timeForAnswer: Int = when {
                     !screenState.isTimerEnabled -> NOT_TO_USE_TIMER
                     input != null && input > 0 -> input
-                    else -> return
+                    else -> {
+                        sendCommand(ShowInvalidEntryError)
+                        return
+                    }
                 }
                 deckSettings.setTimeForAnswer(timeForAnswer)
+                sendCommand(ShowSavedMessage)
                 exercise.notifyExercisePreferenceChanged()
             }
         }
--- app/src/main/kotlin/com/odnovolov/forgetmenot/presentation/screen/motivationaltimer/MotivationalTimerFragment.kt
+++ app/src/main/kotlin/com/odnovolov/forgetmenot/presentation/screen/motivationaltimer/MotivationalTimerFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver
+import android.view.inputmethod.EditorInfo
 import androidx.core.view.isVisible
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.odnovolov.forgetmenot.R
@@ -14,6 +15,8 @@ import com.odnovolov.forgetmenot.presentation.common.mainactivity.MainActivity
 import com.odnovolov.forgetmenot.presentation.screen.deckeditor.decksettings.DeckSettingsDiScope
 import com.odnovolov.forgetmenot.presentation.screen.example.ExampleExerciseDiScope
 import com.odnovolov.forgetmenot.presentation.screen.example.ExampleExerciseFragment
+import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerController.Command.ShowInvalidEntryError
+import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerController.Command.ShowSavedMessage
 import com.odnovolov.forgetmenot.presentation.screen.motivationaltimer.MotivationalTimerEvent.*
 import kotlinx.android.synthetic.main.fragment_motivational_timer.*
 import kotlinx.coroutines.launch
@@ -45,6 +48,7 @@ class MotivationalTimerFragment : BaseFragment() {
             controller = diScope.controller
             viewModel = diScope.viewModel
             observeViewModel()
+            controller!!.commands.observe(::executeCommand)
         }
     }
 
@@ -58,10 +62,20 @@ class MotivationalTimerFragment : BaseFragment() {
             controller?.dispatch(HelpButtonClicked)
         }
         timerSwitchFrame.setOnClickListener {
+            savedTextView.isVisible = false
             controller?.dispatch(TimeForAnswerSwitchToggled)
         }
         timeForAnswerEditText.observeText { text: String ->
+            savedTextView.isVisible = false
             controller?.dispatch(TimeInputChanged(text))
+        }
+        timeForAnswerEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                controller?.dispatch(OkButtonClicked)
+                true
+            } else {
+                false
+            }
         }
         okButton.setOnClickListener {
             controller?.dispatch(OkButtonClicked)
@@ -83,17 +97,38 @@ class MotivationalTimerFragment : BaseFragment() {
                 }
                 secTextView.isEnabled = isTimerEnabled
                 timeForAnswerEditText.run {
-                    isEnabled = isTimerEnabled
                     if (isTimerEnabled) {
+                        isEnabled = true
                         selectAll()
                         showSoftInput()
                     } else {
                         setSelection(0)
+                        hideSoftInput()
+                        isEnabled = false
                     }
                 }
             }
             isOkButtonVisible.observe { isVisible: Boolean ->
                 okButton.isVisible = isVisible
+            }
+        }
+    }
+
+    private fun executeCommand(command: MotivationalTimerController.Command) {
+        when (command) {
+            ShowInvalidEntryError -> {
+                timeForAnswerEditText.error = getString(R.string.error_motivational_timer_input)
+            }
+            ShowSavedMessage -> {
+                savedTextView.alpha = 1f
+                savedTextView.isVisible = true
+                savedTextView.animate()
+                    .setStartDelay(1000)
+                    .setDuration(1500)
+                    .alpha(0f)
+                    .start()
+                timeForAnswerEditText.hideSoftInput()
+                timeForAnswerEditText.clearFocus()
             }
         }
     }
