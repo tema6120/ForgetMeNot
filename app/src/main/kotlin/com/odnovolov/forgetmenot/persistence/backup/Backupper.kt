1d8e0ea1e156c8916b8b899d4cad4b39b559b1d9
Odnovolov Artem <odnovolov.artem@gmail.com>
2023-04-02 23:52:24 +0300

    implemented rollback in case if backup import fails

--- app/src/main/kotlin/com/odnovolov/forgetmenot/persistence/backup/Backupper.kt
+++ app/src/main/kotlin/com/odnovolov/forgetmenot/persistence/backup/Backupper.kt
@@ -1,52 +1,103 @@
 package com.odnovolov.forgetmenot.persistence.backup
 
+import com.odnovolov.forgetmenot.Database
 import com.odnovolov.forgetmenot.persistence.*
 import com.odnovolov.forgetmenot.presentation.common.di.AppDiScope
-import java.io.BufferedOutputStream
-import java.io.File
-import java.io.InputStream
-import java.io.OutputStream
+import com.squareup.sqldelight.db.SqlCursor
+import java.io.*
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
 
 class Backupper {
+    private val app = AppDiScope.get().app
+
+    private val databaseDir: File by lazy {
+        app.getDatabasePath(DATABASE_NAME).parentFile!!
+    }
+
+    private val databaseDirInCacheDir: File by lazy {
+        File(app.cacheDir, databaseDir.name)
+    }
+
     fun export(outputStream: OutputStream): Boolean {
         return try {
-            ZipOutputStream(BufferedOutputStream(outputStream)).use { out ->
-                AppDiScope.get().app.getDatabasePath(DATABASE_NAME).parentFile!!.listFiles()!!
-                    .forEach { file: File ->
-                        out.putNextEntry(ZipEntry(file.name))
-                        file.inputStream().use { origin ->
-                            origin.copyTo(out, 1024)
-                        }
-                    }
-            }
+            zipBackup(outputStream)
             true
         } catch (e: Exception) {
             false
         }
     }
 
-    fun import(inputStream: InputStream): Boolean {
-        return try {
-            AppDiScope.get().sqlDriver.close()
-            val databaseDir: File = AppDiScope.get().app.getDatabasePath(DATABASE_NAME).parentFile!!
-            // todo: make copy of existing database to restore data in case of failure
-            databaseDir.listFiles()!!.forEach(File::delete)
-            ZipInputStream(inputStream).use { zipInputStream: ZipInputStream ->
-                while (true) {
-                    val zipEntry = zipInputStream.nextEntry ?: break
-                    val newFile = File(databaseDir, zipEntry.name)
-                    newFile.createNewFile()
-                    newFile.outputStream().use { out ->
-                        zipInputStream.copyTo(out, 1024)
-                    }
+    private fun zipBackup(outputStream: OutputStream) {
+        ZipOutputStream(BufferedOutputStream(outputStream)).use { zipOutputStream: ZipOutputStream ->
+            databaseDir.listFiles()!!.forEach { file: File ->
+                zipOutputStream.putNextEntry(ZipEntry(file.name))
+                file.inputStream().use { fileInputStream: FileInputStream ->
+                    fileInputStream.copyTo(zipOutputStream, 1024)
                 }
             }
+        }
+    }
+
+    fun import(inputStream: InputStream): Boolean {
+        return try {
+            closeDatabase()
+            moveCurrentDatabaseToCache()
+            unzipBackup(inputStream)
+            validateNewDatabase()
             true
         } catch (e: Exception) {
+            rollback()
             false
+        } finally {
+            clearCache()
         }
+    }
+
+    private fun closeDatabase() {
+        AppDiScope.get().sqlDriver.close()
+    }
+
+    private fun moveCurrentDatabaseToCache() {
+        clearCache()
+        databaseDirInCacheDir.mkdir()
+        databaseDir.copyRecursively(databaseDirInCacheDir, overwrite = true)
+        databaseDir.listFiles()!!.forEach(File::delete)
+    }
+
+    private fun unzipBackup(inputStream: InputStream) {
+        ZipInputStream(inputStream).use { zipInputStream: ZipInputStream ->
+            while (true) {
+                val zipEntry = zipInputStream.nextEntry ?: break
+                val newFile = File(databaseDir, zipEntry.name)
+                newFile.createNewFile()
+                newFile.outputStream().use { fileOutputStream: FileOutputStream ->
+                    zipInputStream.copyTo(fileOutputStream, 1024)
+                }
+            }
+        }
+    }
+
+    private fun validateNewDatabase() {
+        val sqlDriver = DatabaseInitializer.initSqlDriver(app)
+        val cursor: SqlCursor =
+            DatabaseInitializer.initSqlDriver(app).executeQuery(null, "PRAGMA schema_version", 0)
+        cursor.next()
+        val schemaVersion = cursor.getLong(0)!!
+        if (schemaVersion <= 1L) throw Exception("Not valid database file")
+
+        val database: Database = DatabaseInitializer.initDatabase(sqlDriver)
+        database.keyValueQueries
+            .selectValue(DbKeys.ARE_INITIAL_DECKS_ADDED)
+            .executeAsOne() // it will throw NullPointerException if there is no value
+    }
+
+    private fun rollback() {
+        databaseDirInCacheDir.copyRecursively(databaseDir, overwrite = true)
+    }
+
+    private fun clearCache() {
+        databaseDirInCacheDir.deleteRecursively()
     }
 }
